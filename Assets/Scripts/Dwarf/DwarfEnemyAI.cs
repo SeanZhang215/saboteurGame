2de528ede0f545a62252be87e484b79285d70368
xinlu <xinlu.zhao@gatech.edu>
2023-03-16 11:37:55 -0700

    AI initial implement

--- Assets/Scripts/Dwarf/DwarfEnemyAI.cs
+++ Assets/Scripts/Dwarf/DwarfEnemyAI.cs
@@ -5,34 +5,69 @@ using UnityEngine.AI;
 
 public class DwarfEnemyAI : MonoBehaviour
 {
-    public GameObject[] waypoints;
-    private int currWaypoint = -1;
     private NavMeshAgent navMeshAgent;
-
     private Animator animator;
+    private GameObject currentStar;
+    private bool isDestroyingObject = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        setNextWaypoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && !navMeshAgent.pathPending)
+        if (currentStar == null)
         {
-            setNextWaypoint();
+            FindNearestStar();
+        }
+        else if (!isDestroyingObject && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && !navMeshAgent.pathPending)
+        {
+            StartCoroutine(DestroyAfterDelay(currentStar, 10f));
+            currentStar = null;
+            FindNearestStar();
         }
         float speed = navMeshAgent.speed;
-        animator.SetBool("isRunning", speed > 0f);
+        if (currentStar == null){
+            speed = 0f;
+        }
+        animator.SetBool("isWalking", speed > 0f);
     }
 
-    void setNextWaypoint()
+    IEnumerator DestroyAfterDelay(GameObject obj, float delay)
     {
-        currWaypoint = (currWaypoint + 1) % waypoints.Length;
-        navMeshAgent.SetDestination(waypoints[currWaypoint].transform.position);
+        isDestroyingObject = true;
+        animator.SetBool("isDestroying", true);
+        yield return new WaitForSeconds(delay);
+        animator.SetBool("isDestroying", false);
+        Destroy(obj);
+        isDestroyingObject = false;
+    }
+
+    void FindNearestStar()
+    {
+        GameObject[] stars = GameObject.FindGameObjectsWithTag("Star");
+        if (stars.Length > 0)
+        {
+            GameObject nearestStar = stars[0];
+            float minDistance = Vector3.Distance(transform.position, nearestStar.transform.position);
+
+            for (int i = 1; i < stars.Length; i++)
+            {
+                float distance = Vector3.Distance(transform.position, stars[i].transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestStar = stars[i];
+                }
+            }
+
+            currentStar = nearestStar;
+            navMeshAgent.SetDestination(currentStar.transform.position);
+        }
     }
 }
--- Assets/Scripts/Dwarf/DwarfTeamAI.cs
+++ Assets/Scripts/Dwarf/DwarfTeamAI.cs
@@ -6,32 +6,41 @@ using UnityEngine.AI;
 public class DwarfTeamAI : MonoBehaviour
 {
     public GameObject[] waypoints;
+    public GameObject starPrefab;
+    public float starDropChance = 0.5f; // Chance of dropping a star at each waypoint
+    public float timeInterval = 2f; // Time interval for dropping stars
     private int currWaypoint = -1;
     private NavMeshAgent navMeshAgent;
     private Animator animator;
+    private float timeSinceLastDrop = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        // setNextWaypoint();
-        
+        SetNextWaypoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && !navMeshAgent.pathPending)
-        // {
-        //     setNextWaypoint();
-        // }
-        // float speed = navMeshAgent.speed;
-        // animator.SetBool("isWalking", speed > 0f);
-        
+        if (Random.value <= starDropChance && Time.time - timeSinceLastDrop >= timeInterval)
+        {
+            Vector3 starPosition = transform.position;
+            starPosition.y += 1.0f; // add 1 unit to Y-coordinate
+            Instantiate(starPrefab, starPosition, Quaternion.identity);
+            timeSinceLastDrop = Time.time;
+        }
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && !navMeshAgent.pathPending)
+        {
+            SetNextWaypoint();            
+        }
+        float speed = navMeshAgent.speed;
+        animator.SetBool("isRunning", speed > 0f);
     }
 
-    void setNextWaypoint()
+    void SetNextWaypoint()
     {
         currWaypoint = (currWaypoint + 1) % waypoints.Length;
         navMeshAgent.SetDestination(waypoints[currWaypoint].transform.position);
